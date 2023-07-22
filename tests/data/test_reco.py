0aca18e1bad20d638be24134f611e29b27fa7468
retarfi <valerososoccer@gmail.com>
2023-07-22 23:17:01 +0900

    fix: path

--- tests/data/test_reco.py
+++ tests/data/test_reco.py
@@ -9,4 +9,4 @@ THIS_DIR: str = os.path.dirname(os.path.abspath(__file__))
 
 @pytest.mark.parametrize("json_file", ("train.json", "test.json"))
 def test_load_reco_datasets(json_file: str) -> None:
-    _ = load_reco_dataset(os.path.join(THIS_DIR, "../../../data/reco/", json_file))
+    _ = load_reco_dataset(os.path.join(THIS_DIR, "../../data/reco/", json_file))
